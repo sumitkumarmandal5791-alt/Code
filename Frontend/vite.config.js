e1ebd84883dfcb7db2d0611ee8b06bfd75e3929f
Sumit <sumitkumarmandal5791@gmail.com>
2026-02-07 21:34:16 +0530

    fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [tailwindcss(), react()],
 
   define: {
     'process.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL),
