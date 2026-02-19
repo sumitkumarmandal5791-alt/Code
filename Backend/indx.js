17603589aa9b337094ab5f07b79ed3e73049b766
Sumit <sumitkumarmandal5791@gmail.com>
2026-02-19 23:20:43 +0530

    fix: cors issue

--- Backend/indx.js
+++ Backend/indx.js
@@ -18,7 +18,6 @@ app.use(cors({
     origin: [allowedOrigin, process.env.CLOUD_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 
